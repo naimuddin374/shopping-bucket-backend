6fb29d827afc418bc8876468162f3d0d8c47d289
Naim Uddin <naim@ticonsys.com>
2021-05-11 13:48:48 +0600

    Project scaffold complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, '../', 'public')))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to our application!' })
+    return res.status(200).json({ message: 'Welcome to our application!' })
 })
 
-app.get('*', (req, res) => {
-    res.status(200).json({ message: '404 not found!' })
+app.use((req, res, next) => {
+    const error = new Error('404 Page Not Found!')
+    error.status = 404
+    next(error)
 })
 
-app.listen(8080, () => {
-    console.log('Server listening on port 8080')
+app.use((error, req, res, next) => {
+    if (error.status === 404) {
+        return res.status(404).json({
+            message: error.message,
+            status: 404
+        })
+    }
+
+    return res.status(500).json({
+        message: 'Internal Server Error!',
+        status: 500
+    })
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`)
 })
